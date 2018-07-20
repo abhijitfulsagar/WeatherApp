880d5794c02dc96ca885eb934a5d924b536ed064
Abhijit <abhijitfulsagar@github.com>
2018-07-19 21:53:59 -0700

    encoding user input

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-var request = require('request');
+const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+	.options({
+		a:{
+			demand:true,
+			alias:'address',
+			describe:'Get the weathe for the given address',
+			string:true
+		}
+	})
+	.help()
+	.alias('help','h')
+	.argv;
+
+console.log("argv:",argv);
+var encodedAddress = encodeURIComponent(argv.address);
+console.log('Encoded Address:',encodedAddress);
+// &key=AIzaSyAy4vkQtxurJyc2_eNKubjcIvANLh3BZM0
 
 request({
-	url:'https://maps.googleapis.com/maps/api/geocode/json?address=80%20chapin%20st%20binghamton&key=AIzaSyAy4vkQtxurJyc2_eNKubjcIvANLh3BZM0',
+	url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json:true
 },(error,response,body)=>{
 	console.log(`Address:${body.results[0].formatted_address}`);
